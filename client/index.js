156a40f9f14b24cf9117b91d3f796ccdc7482c39
iliacodes <6ac177ed@protonmail.com>
2023-04-12 08:49:41 -0700

    completed week2 project.

--- client/index.js
+++ client/index.js
@@ -7,8 +7,18 @@ const serverUrl = 'http://localhost:1225';
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
 
+  const tree = new MerkleTree(niceList);
+
+  console.log(process.argv[2])
+  const name = process.argv[2];
+  // const name = "Ruben Toy"
+  const index = niceList.findIndex(n => n === name);
+  const proof = tree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+
+    name, 
+    proof,
   });
 
   console.log({ gift });
